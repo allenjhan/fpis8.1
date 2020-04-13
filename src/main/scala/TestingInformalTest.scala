2b212e8a3246c9c47dde960d5b0c0e0095e89023
Allen Han <allenjhan@gmail.com>
2020-04-13 11:45:08 -0700

    initial

---
+++ src/main/scala/TestingInformalTest.scala
@@ -0,0 +1,10 @@
+object TestingInformalTest {
+  /*
+  sum of a list and a reversed list should be the same
+  sum of a list of identical elements should be equal to multiplying one element by the number of elements
+  the sum of the sum of the first half and the sum of the second half should equal the total sum
+  the sum of the sorted list should equal the sum of the unsorted list
+  scrambling the list should yield the same sum as the original
+   */
+
+}
